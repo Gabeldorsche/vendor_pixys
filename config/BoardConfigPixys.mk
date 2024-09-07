4a5f25ab42b085e5c5ecf6cf66f8111079268ce1
aswin7469 <aswinas@pixysos.com>
2024-09-07 22:59:32 +0530

    [DNM] kang/update kernel build tools from los
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- config/BoardConfigPixys.mk
+++ config/BoardConfigPixys.mk
@@ -1,12 +1,13 @@
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Recovery
+BOARD_USES_FULL_RECOVERY_IMAGE ?= true
+
 include vendor/pixys/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include hardware/qcom-caf/common/BoardConfigQcom.mk
+    include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/pixys/config/BoardConfigSoong.mk
-
-# Namespace for fwk-detect
-TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
-PRODUCT_SOONG_NAMESPACES += \
-    $(TARGET_FWK_DETECT_PATH)/fwk-detect
